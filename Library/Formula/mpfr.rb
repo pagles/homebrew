ac2da789dc885f3ef4e0399052b4a565e16395e6
Emmanuel Blot <eblot@moaningmarmot.info>
2010-01-13 23:57:15 +0100

    Add MPFR library

---
+++ Library/Formula/mpfr.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Mpfr <Formula
+  @url='http://www.mpfr.org/mpfr-current/mpfr-2.4.2.tar.bz2'
+  @homepage='http://www.mpfr.org/'
+  @sha1='7ca93006e38ae6e53a995af836173cf10ee7c18c'
+
+  depends_on 'gmp'
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-gmp=#{Formula.factory('gmp').prefix}"
+    system "make"
+    system "make install"
+  end
+end
