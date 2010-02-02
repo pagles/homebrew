75ad5cf149afa90ba590702fa83d31cf27461417
Emmanuel Blot <eblot@neotion.com>
2010-02-02 11:56:27 +0100

    Cadaver seems to rely on gettext (and fails to build w/o brew gettext)

--- Library/Formula/cadaver.rb
+++ Library/Formula/cadaver.rb
@@ -5,9 +5,12 @@ class Cadaver <Formula
   homepage 'http://www.webdav.org/cadaver/'
   md5 '5ac79e41f3b7b4f68bf4003beed5dc26'
 
+  depends_on 'gettext'
+  
   def install
+    # ENV.deparallelize
+    
     system "./configure", "--prefix=#{prefix}", "--disable-debug",
-                          "--disable-dependency-tracking",
                           "--with-ssl"
     system "(cd lib/intl; make)"
     system "make install"
