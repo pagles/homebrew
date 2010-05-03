2cc8b489447179c3844bf9b669797aa0d5b2b987
Emmanuel Blot <eblot@neotion.com>
2010-05-03 19:35:17 +0200

    Enable 3Mbps to 12Mbps UART bitrate with 2232H and 4232H devices

--- Library/Formula/libftdi.rb
+++ Library/Formula/libftdi.rb
@@ -25,8 +25,8 @@ class Libftdi <Formula
 end
 
 __END__
---- a/CMakeLists.txt	2009-12-24 12:09:27.000000000 +0100
-+++ b/CMakeLists.txt	2009-12-24 12:11:07.000000000 +0100
+--- a/CMakeLists.txt	2009-12-19 16:07:25.000000000 +0100
++++ b/CMakeLists.txt	2010-05-03 19:30:34.000000000 +0200
 @@ -38,14 +38,8 @@
  set(CPACK_COMPONENT_STATICLIBS_GROUP "Development")
  set(CPACK_COMPONENT_HEADERS_GROUP    "Development")
@@ -51,3 +51,141 @@ __END__
 -add_subdirectory(bindings)
 -add_subdirectory(examples)
  add_subdirectory(packages)
+ 
+ 
+--- a/src/ftdi.c	2009-12-19 15:52:50.000000000 +0100
++++ b/src/ftdi.c	2010-05-03 19:30:00.000000000 +0200
+@@ -51,6 +51,9 @@
+    } while(0);
+ 
+ 
++#define FTDI_BAUDRATE_REF_CLOCK 3000000 /* 3 MHz */
++#define FTDI_BAUDRATE_TOLERANCE       3 /* acceptable clock drift, in % */
++
+ /**
+     Internal function to close usb device pointer.
+     Sets ftdi->usb_dev to NULL.
+@@ -914,14 +917,28 @@
+     int divisor, best_divisor, best_baud, best_baud_diff;
+     unsigned long encoded_divisor;
+     int i;
+-
++    unsigned int ref_clock = FTDI_BAUDRATE_REF_CLOCK;
++    int hispeed = 0;
++    
+     if (baudrate <= 0)
+     {
+         // Return error
+         return -1;
+     }
+ 
+-    divisor = 24000000 / baudrate;
++    if ( (ftdi->type == TYPE_4232H) || (ftdi->type == TYPE_2232H) )
++    {
++        // these chips can support a 12MHz clock in addition to the original
++        // 3MHz clock. This allows higher baudrate (up to 12Mbps) and more
++        // precise baudrates for baudrate > 3Mbps/2
++        if ( baudrate > (FTDI_BAUDRATE_REF_CLOCK>>1) )
++        {
++            ref_clock *= 4; // 12 MHz
++            hispeed = 1;
++        }
++    }
++    
++    divisor = (ref_clock<<3) / baudrate;
+ 
+     if (ftdi->type == TYPE_AM)
+     {
+@@ -939,45 +956,49 @@
+         int baud_estimate;
+         int baud_diff;
+ 
+-        // Round up to supported divisor value
+-        if (try_divisor <= 8)
+-        {
+-            // Round up to minimum supported divisor
+-            try_divisor = 8;
+-        }
+-        else if (ftdi->type != TYPE_AM && try_divisor < 12)
++        if ( ! hispeed )
+         {
+-            // BM doesn't support divisors 9 through 11 inclusive
+-            try_divisor = 12;
+-        }
+-        else if (divisor < 16)
+-        {
+-            // AM doesn't support divisors 9 through 15 inclusive
+-            try_divisor = 16;
+-        }
+-        else
+-        {
+-            if (ftdi->type == TYPE_AM)
++            // Round up to supported divisor value
++            if (try_divisor <= 8)
+             {
+-                // Round up to supported fraction (AM only)
+-                try_divisor += am_adjust_up[try_divisor & 7];
+-                if (try_divisor > 0x1FFF8)
+-                {
+-                    // Round down to maximum supported divisor value (for AM)
+-                    try_divisor = 0x1FFF8;
+-                }
++                // Round up to minimum supported divisor
++                try_divisor = 8;
++            }
++            else if (ftdi->type != TYPE_AM && try_divisor < 12)
++            {
++                // BM doesn't support divisors 9 through 11 inclusive
++                try_divisor = 12;
++            }
++            else if ( divisor < 16 )
++            {
++                // AM doesn't support divisors 9 through 15 inclusive
++                try_divisor = 16;
+             }
+             else
+             {
+-                if (try_divisor > 0x1FFFF)
++                if (ftdi->type == TYPE_AM)
+                 {
+-                    // Round down to maximum supported divisor value (for BM)
+-                    try_divisor = 0x1FFFF;
++                    // Round up to supported fraction (AM only)
++                    try_divisor += am_adjust_up[try_divisor & 7];
++                    if (try_divisor > 0x1FFF8)
++                    {
++                        // Round down to maximum supported divisor value (for AM)
++                        try_divisor = 0x1FFF8;
++                    }
++                }
++                else
++                {
++                    if (try_divisor > 0x1FFFF)
++                    {
++                        // Round down to maximum supported divisor value (for BM)
++                        try_divisor = 0x1FFFF;
++                    }
+                 }
+             }
+         }
++
+         // Get estimated baud rate (to nearest integer)
+-        baud_estimate = (24000000 + (try_divisor / 2)) / try_divisor;
++        baud_estimate = ((ref_clock<<3) + (try_divisor / 2)) / try_divisor;
+         // Get absolute difference from requested baud rate
+         if (baud_estimate < baudrate)
+         {
+@@ -1020,7 +1041,13 @@
+         *index |= ftdi->index;
+     }
+     else
++    {
+         *index = (unsigned short)(encoded_divisor >> 16);
++    }
++    if ( hispeed )
++    {
++        *index |= 1<<9; // use hispeed mode
++    }
+ 
+     // Return the nearest baud rate
+     return best_baud;
