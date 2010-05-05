b480cc714c5d7fd4e95dc80ccf566c08f492bcd0
Emmanuel Blot <eblot.ml@gmail.com>
2010-05-05 12:37:09 +0200

    Add MPC library (required dependency for GCC 4.5+)

--- Library/Formula/libmpc.rb
+++ Library/Formula/libmpc.rb
@@ -1,16 +1,19 @@
 require 'formula'
 
-class Libmpc < Formula
-  url 'http://multiprecision.org/mpc/download/mpc-0.8.1.tar.gz'
-  homepage 'http://multiprecision.org'
-  md5 '5b34aa804d514cc295414a963aedb6bf'
+class Libmpc <Formula
+  @url='http://www.multiprecision.org/mpc/download/mpc-0.8.1.tar.gz'
+  @homepage='http://www.multiprecision.org/'
+  @sha1='5ef03ca7aee134fe7dfecb6c9d048799f0810278'
 
   depends_on 'gmp'
   depends_on 'mpfr'
 
   def install
-    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make"
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-gmp=#{Formula.factory('gmp').prefix}",
+                          "--with-mpfr=#{Formula.factory('mpfr').prefix}",
+                          "--disable-debug"
+                          
     system "make install"
   end
 end
