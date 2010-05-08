b4c061d02bee9b199f152999c448c24c0b1b391e
Emmanuel Blot <eblot@moaningmarmot.info>
2010-05-08 22:55:00 +0200

    Upgrade libusb to 1.0.8

--- Library/Formula/libusb.rb
+++ Library/Formula/libusb.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Libusb <Formula
-  @url='http://downloads.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.7/libusb-1.0.7.tar.bz2'
+  @url='http://downloads.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.8/libusb-1.0.8.tar.bz2'
   @homepage='http://libusb.sourceforge.net'
-  @md5='31660f3433ba40c57be31b8a7d709a7d'
+  @sha1='5484397860f709c9b51611d224819f8ed5994063'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
