e02469026853f3ac8c54f703220737a0e74178fe
Emmanuel Blot <eblot@moaningmarmot.info>
2010-06-11 00:24:36 +0200

    Update from Homebrew main branch

--- Library/Formula/nginx.rb
+++ Library/Formula/nginx.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Nginx < Formula
   url 'http://nginx.org/download/nginx-0.7.65.tar.gz'
-  head 'http://nginx.org/download/nginx-0.8.36.tar.gz'
+  head 'http://nginx.org/download/nginx-0.8.39.tar.gz'
   homepage 'http://nginx.org/'
 
   if ARGV.include? '--HEAD'
-    @md5='171d88e44be3025a0bdac48c4eecd743'
+    @md5='87e88c68b7651ab6aa879b6a563c3bd0'
   else
     @md5='abc4f76af450eedeb063158bd963feaa'
   end
