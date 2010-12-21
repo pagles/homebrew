127429fcb02ad52b9843be5a95b32ca9e3adaa27
Emmanuel Blot <eblot@neotion.com>
2010-12-21 12:43:51 +0100

    Upgrade GDB for Arm eCos to v7.2

--- Library/Formula/gdb-arm-ecos.rb
+++ Library/Formula/gdb-arm-ecos.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class GdbArmEcos <Formula
-  @url='http://ftp.gnu.org/gnu/gdb/gdb-7.1.tar.bz2'
+  @url='http://ftp.gnu.org/gnu/gdb/gdb-7.2.tar.bz2'
   @homepage='http://www.gnu.org/software/gdb/'
-  @sha1='417e2e637a296ea0e1cdddf56233311b8708fa19'
+  @sha1='cae138dee0c11778c471a1d5e4b09e0ae08f9e9d'
 
   depends_on 'gmp'
   depends_on 'mpfr'
@@ -11,6 +11,8 @@ class GdbArmEcos <Formula
   depends_on 'cloog-ppl'
 
   def install
+    # Cannot build with LLVM (cross compiler crashes)
+    ENV.gcc_4_2
     system "./configure", "--prefix=#{prefix}", "--target=arm-eabi",
                 "--with-gmp=#{Formula.factory('gmp').prefix}",
                 "--with-mpfr=#{Formula.factory('mpfr').prefix}",
