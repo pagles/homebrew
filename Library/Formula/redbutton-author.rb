733edacdaa631dc0b8aa8d72612b48e9d3ff4f18
Emmanuel Blot <eblot@moaningmarmot.info>
2011-01-29 22:23:27 +0100

    Fix up the redbutton-author Makefile for installation path, as the original Makefile used to hardcode the destination path

--- Library/Formula/redbutton-author.rb
+++ Library/Formula/redbutton-author.rb
@@ -5,12 +5,27 @@ class RedbuttonAuthor <Formula
   homepage 'http://redbutton.sourceforge.net/'
   md5 '373ce10d1de8a6b84782818b0b1f54e4'
 
-  # depends_on 'cmake'
+  def patches
+    DATA
+  end
 
   def install
     # badly written Makefile
     ENV.deparallelize
     system "make"
-    system "make install"
+    system "make install INSTALLDIR=#{prefix}"
   end
 end
+
+__END__
+--- a/Makefile	2009-07-27 15:52:12.000000000 +0200
++++ b/Makefile	2011-01-29 22:12:01.000000000 +0100
+@@ -11,7 +11,7 @@
+ #LEX=lex
+ #YACC=yacc
+ 
+-DESTDIR=/usr/local
++DESTDIR=$(INSTALLDIR)
+ 
+ MHEGC_OBJS=	mhegc.o	\
+ 		lex.parser.o	\
