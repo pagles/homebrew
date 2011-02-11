8f6d1c06e24e59bb3610d3f333d8045e25d65a56
Emmanuel Blot <eblot@neotion.com>
2011-02-11 12:21:04 +0100

    redbutton-author: fix up installation path

--- Library/Formula/redbutton-author.rb
+++ Library/Formula/redbutton-author.rb
@@ -6,11 +6,11 @@ class RedbuttonAuthor <Formula
   md5 '373ce10d1de8a6b84782818b0b1f54e4'
 
   def patches
+    # Original Makefile hardcode the destination path...
     DATA
   end
 
   def install
-    # badly written Makefile
     ENV.deparallelize
     system "make"
     system "make install INSTALLDIR=#{prefix}"
@@ -19,7 +19,7 @@ end
 
 __END__
 --- a/Makefile	2009-07-27 15:52:12.000000000 +0200
-+++ b/Makefile	2011-01-29 22:12:01.000000000 +0100
++++ b/Makefile	2011-02-11 12:17:05.000000000 +0100
 @@ -11,7 +11,7 @@
  #LEX=lex
  #YACC=yacc
@@ -29,3 +29,11 @@ __END__
  
  MHEGC_OBJS=	mhegc.o	\
  		lex.parser.o	\
+@@ -56,6 +56,7 @@
+ 	${CC} ${CFLAGS} ${DEFS} -o berdecode berdecode.c
+ 
+ install:	mhegc mhegd
++	mkdir -p ${DESTDIR}/bin
+ 	install -m 755 mhegc ${DESTDIR}/bin
+ 	install -m 755 mhegd ${DESTDIR}/bin
+ 
